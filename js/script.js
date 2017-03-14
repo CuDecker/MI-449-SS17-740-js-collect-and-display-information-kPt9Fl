fce2aaea1ac34045fe748af24b7f4c09bc3066c9
CuDecker <deckercu@msu.edu>
2017-03-14 17:14:04 -0400

    consolidated some javascript

--- js/script.js
+++ js/script.js
@@ -6,52 +6,23 @@ var phoneInput = document.getElementById('phone')
 var preview = document.getElementById('preview')
 var html = document.getElementById('html')
 
-var updatePreview = function () {
+var update = function () {
   var firstName = firstNameInput.value
   var lastName = lastNameInput.value
   var description = descriptionInput.value
   var email = emailInput.value
   var phone = phoneInput.value
 
-  preview.innerHTML = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '! </h1>' +
+  var text = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '! </h1>' +
   '<p>' + description + '</p>' +
   '<p> If you\'re interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank">' + email + '</a> or give me a call at <a href="tel:' + phone + '" target="_blank">' + phone + '</a></p>'
+
+  preview.innerHTML = text
+  html.textContent = text
 }
 
-var updateHTML = function () {
-  var firstName = firstNameInput.value
-  var lastName = lastNameInput.value
-  var description = descriptionInput.value
-  var email = emailInput.value
-  var phone = phoneInput.value
-
-  html.textContent = '<h1>Hi, my name is ' + firstName + ' ' + lastName + '! </h1>' +
-  '<p>' + description + '</p>' +
-  '<p> If you\'re interested in a date, you can email me at <a href="mailto:' + email + '" target="_blank">' + email + '</a> or give me a call at <a href="tel:' + phone + '" target="_blank">' + phone + '</a></p>'
-}
-
-firstNameInput.addEventListener('input', function () {
-  updateHTML()
-  updatePreview()
-  console.log('oh nice to meet you ' + firstNameInput.value)
-})
-
-lastNameInput.addEventListener('input', function () {
-  updateHTML()
-  updatePreview()
-})
-
-descriptionInput.addEventListener('input', function () {
-  updateHTML()
-  updatePreview()
-})
-
-emailInput.addEventListener('input', function () {
-  updateHTML()
-  updatePreview()
-})
-
-phoneInput.addEventListener('input', function () {
-  updateHTML()
-  updatePreview()
-})
+firstNameInput.addEventListener('input', update)
+lastNameInput.addEventListener('input', update)
+descriptionInput.addEventListener('input', update)
+emailInput.addEventListener('input', update)
+phoneInput.addEventListener('input', update)
